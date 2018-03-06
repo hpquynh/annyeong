24670de411e7df518c3ea8cf37fe72f43631020b
Quynh HP <hophuongquynh92@gmail.com>
2018-03-06 10:11:43 +0700

    [modify] update api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 // A global API-root
 // export const API_BASE = '../langs.json';
-export const API_BASE = 'http://localhost:1338/list';
+export const API_BASE = 'http://topcbl.online:1338';
 export const LANG_EN = 'en';
 export const LANG_KR = 'kr';
 export const CODE_EN = '1';
