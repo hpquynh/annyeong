d800856d42252fa678b0563b43bb41ac25fbb917
Quynh HP <hophuongquynh92@gmail.com>
2018-03-16 15:25:45 +0700

    [modify] update link api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 // A global API-root
 // export const API_BASE = '../langs.json';
-export const API_BASE = 'http://topcbl.online:1338';
+export const API_BASE = 'http://korean.topcbl.online';
 export const LANG_EN = 'en';
 export const LANG_KR = 'kr';
 export const CODE_EN = '1';
